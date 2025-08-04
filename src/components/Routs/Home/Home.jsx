88f1006b43261d4a2d10e3e16d8cacb46bb044ce
selim2066 <mdselimreza2066@gmail.com>
2025-08-04 18:58:46 +0600

    json

--- src/components/Routs/Home/Home.jsx
+++ src/components/Routs/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../banner/banner"
+import Hero from "../../hero/Hero";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-             <div className="h-[280px]"></div>
+             <div className="h-[380px]"></div>
+            <Hero></Hero>
         </div>
     );
 };
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-center font-bold'>Explore Cutting-Edge Tech Gadget</h1>
+        </div>
+    );
+};
+
+export default Hero;
