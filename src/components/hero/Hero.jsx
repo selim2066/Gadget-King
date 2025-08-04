1041b0fd1406fe66cf3def0194c3f00c2595f19a
selim2066 <mdselimreza2066@gmail.com>
2025-08-04 23:47:56 +0600

    solution is comming

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 import Gadget from "../gadget/Gadget";
 
 const Hero = () => {
@@ -19,10 +21,27 @@ const Hero = () => {
       <div className="flex gap-5">
         <div className="w-1/4 bg-green-500">
           <h1>this is for category</h1>
+          <Tabs>
+            <TabList className="flex flex-col">
+              <Tab>All Product</Tab>
+              <Tab>Laptop</Tab>
+              <Tab>Iphone</Tab>
+              <Tab>MacBook</Tab>
+              <Tab>Accessories</Tab>
+            </TabList>
+
+            <TabPanel>
+              <h2>Any content 1</h2>
+            </TabPanel>
+            <TabPanel>
+              <h2>Any content 2</h2>
+            </TabPanel>
+          </Tabs>
         </div>
         <div className="w-3/4 bg-amber-600 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center p-5">
-          
-          {gadgets.map((gadget) =>  <Gadget key={gadget.product_id} gadget={gadget} />)}
+          {gadgets.map((gadget) => (
+            <Gadget key={gadget.product_id} gadget={gadget} />
+          ))}
         </div>
       </div>
     </div>
