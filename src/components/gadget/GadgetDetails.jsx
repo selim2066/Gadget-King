0eb89fb655a6e931c4cd45923ec16a5de334f61b
selim2066 <mdselimreza2066@gmail.com>
2025-08-06 23:49:44 +0600

    gadget details

--- src/components/gadget/GadgetDetails.jsx
+++ src/components/gadget/GadgetDetails.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useLoaderData, useParams } from 'react-router-dom';
 
 const GadgetDetails = () => {
+    const {product_id}=useParams()
+    const gadgetData = useLoaderData()
+    
     return (
         <div>
             
