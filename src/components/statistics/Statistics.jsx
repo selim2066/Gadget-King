e9689888d701780ec613ede67cb971e41c2e3159
selim2066 <mdselimreza2066@gmail.com>
2025-08-10 11:32:38 +0600

    statistics workings

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,28 +1,42 @@
 import { useContext } from "react";
 import { CartContexts } from "../../context/CartContext";
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from "recharts";
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  CartesianGrid
+} from "recharts";
 
 const Statistics = () => {
-  const { cart } = useContext(CartContexts); 
+  const { cart } = useContext(CartContexts);
 
-  // Prepare chart data: each item will be { name: productName, price: productPrice }
-  const data = cart.map(item => ({
-    name: item.product_title,  // change key if your product name is different
-    price: item.price // change key if your price field is different
+  // Prepare chart data: each item will be
+
+  const data = cart.map((item) => ({
+    name: item.product_title,
+    price: item.price,
   }));
 
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Statistics</h1>
-      
-      <BarChart width={600} height={300} data={data}>
-        <XAxis dataKey="name" stroke="#8884d8" />
-        <YAxis />
-        <Tooltip wrapperStyle={{ backgroundColor: "#ccc" }} />
-        <Legend />
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <Bar dataKey="price" fill="#8884d8" barSize={30} />
-      </BarChart>
+
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data}>
+          <XAxis dataKey="name" stroke="#8884d8" />
+          <YAxis />
+          <Tooltip wrapperStyle={{ backgroundColor: "#ccc" }} />
+          <Legend />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" />
+          <YAxis dataKey="price" />
+          <Bar dataKey="price" fill="#8884d8" barSize={30} />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 };
